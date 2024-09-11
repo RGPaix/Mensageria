fb62dfac0386dc9c07db81dcfe3f15ad80f781a8
RGPaix <161089642+RGPaix@users.noreply.github.com>
2024-09-11 18:08:33 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,47 @@
+package com.mensageria;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Properties;
+import java.util.Scanner;
+
+public class Cliente {
+    public static void main(String[] args){
+        String topic1 = "compra";
+        String topic2 = "status";
+
+        Properties producerProps = new Properties();
+        producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(producerProps);
+
+        Properties consumerProps = new Properties();
+        consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "mensageria-consumer-group");
+        consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(consumerProps);
+
+        String value;
+        Scanner sc = new Scanner(System.in);
+        do {
+            value = sc.nextLine();
+            ProducerRecord<String, String> record = new ProducerRecord<>(topic1, value);
+            producer.send(record);
+            System.err.println("STATUS DA COMPRA ---->> " + value);
+        } while (value != "0");
+        producer.close();
+    }
+}
+
+
